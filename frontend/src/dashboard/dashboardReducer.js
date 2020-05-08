bc7f217d456be238426c591831e1c57e8b00b0d7
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-08 15:53:00 -0300

    Dashboard action creator

--- frontend/src/dashboard/dashboardReducer.js
+++ frontend/src/dashboard/dashboardReducer.js
@@ -5,6 +5,13 @@ const INITIAL_STATE = {
     }
 }
 
-export default function (state, INITIAL_STATE, action) {
-    return state;
+export default function (state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case 'BILLING_SUMMARY_FETCHED':
+            return {
+                ...state, summary: action.payload.data
+            }
+            default:
+                return state
+    }
 }
