19d1028fee4f950c82e2705b0442e81ecd74d4c6
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-08 19:02:13 -0300

    Billing cycle list connect redux

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { getList } from "./billingCycleActions";
 
 class BillingCycleList extends Component {
+  componentWillMount() {
+    this.props.getList();
+  }
   render() {
     return (
       <div>
@@ -19,4 +25,7 @@ class BillingCycleList extends Component {
   }
 }
 
-export default BillingCycleList;
+const mapStateToProps = (state) => ({ list: state.billingCycle.list });
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ getList }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
