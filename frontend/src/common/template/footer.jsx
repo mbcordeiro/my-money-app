37d913ea3a4801e4b2b2e721f63d56fda43064a1
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-08 11:07:10 -0300

    Footer component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2020
+      <a href="https://github.com/matheusBCordeiro" target="_blank">
+        {" "}
+        Matheus Cordeiro
+      </a>
+      .
+    </strong>
+  </footer>
+);
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import Header from "../common/template/header";
 import SideBar from "../common/template/sideBar";
-
+import Footer from "../common/template/footer";
 import "../common/template/dependencies";
 
 export default (props) => {
   <div className="wrapper">
     <Header />
+    <SideBar />
+    <div className="content-wrapper"></div>
+    <Footer />
   </div>;
 };
