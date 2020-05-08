b6debe1a31a0afdb5bed8be4c17ba4e74f0aeb78
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-08 11:16:17 -0300

    React-router

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import Header from "../common/template/header";
 import SideBar from "../common/template/sideBar";
 import Footer from "../common/template/footer";
+import Routes from "./routes";
 import "../common/template/dependencies";
 
 export default (props) => {
   <div className="wrapper">
     <Header />
     <SideBar />
-    <div className="content-wrapper"></div>
+    <div className="content-wrapper">
+      <Routes />
+    </div>
     <Footer />
   </div>;
 };
