242fa5845428e253337042a773571435a438bfaf
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-08 18:21:01 -0300

    Billing cycles action creator and reducers

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -4,10 +4,12 @@ import {
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
+    billingCycle: BillingCycleReducer,
 })
 
 export default rootReducer
