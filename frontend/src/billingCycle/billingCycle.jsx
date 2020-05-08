b6f1f0b79c38a249d7847c9e7251cc7d57e02e4d
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-08 17:36:53 -0300

    Billing cycle connect redux

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
@@ -6,8 +8,12 @@ import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
-import TabContent from '../common/tab/tabContent'
+import TabContent from "../common/tab/tabContent";
+import { selectTab } from "../common/tab/tabActions";
 class BillingCycle extends Component {
+  componentWillMount() {
+    this.props.init();
+  }
   render() {
     return (
       <div>
@@ -21,10 +27,10 @@ class BillingCycle extends Component {
               <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
             </TabsHeader>
             <TabsContent>
-            <TabContent id='tabList'></TabContent>
-            <TabContent id='tabCreate'></TabContent>
-            <TabContent id='tabUpdate'></TabContent>
-            <TabContent id='tabDelete'></TabContent>
+              <TabContent id="tabList"></TabContent>
+              <TabContent id="tabCreate"></TabContent>
+              <TabContent id="tabUpdate"></TabContent>
+              <TabContent id="tabDelete"></TabContent>
             </TabsContent>
           </Tabs>
         </Content>
@@ -32,3 +38,6 @@ class BillingCycle extends Component {
     );
   }
 }
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({});
+export default connect(null, mapDispatchToProps)(BillingCycle);
