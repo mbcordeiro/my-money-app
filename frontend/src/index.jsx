5ec8140e8fb43c851cd29740596292c54dd4b7ad
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-08 14:19:16 -0300

    Redux config

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './main/app';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+import App from "./main/app";
+import reducers from "./main/reducers";
+
+const store = createStore(reducers);
 ReactDOM.render(
-//  <React.StrictMode>
-    <App />,
-// </React.StrictMode>,
-  document.getElementById('app')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
 );
 
 //serviceWorker.unregister();
---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,14 @@
+import {
+    combineReducers
+} from 'redux';
+
+const rootReducer = combineReducers({
+    dashboard: () => ({
+        summary: {
+            credit: 100,
+            debt: 50
+        }
+    })
+})
+
+export default rootReducer
