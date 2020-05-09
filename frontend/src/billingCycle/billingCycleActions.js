37ea81f4d654aaa97c9a84f862951e16333e579b
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-09 00:50:05 -0300

    Refactoring create billing cycle

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -2,6 +2,14 @@ import axios from 'axios'
 import {
     toastr
 } from 'react-redux-toastr'
+import {
+    reset as resetForm,
+    initialize
+} from 'redux-form'
+import {
+    showTabs,
+    selectTab
+} from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -13,15 +21,20 @@ export function getList() {
     }
 
     export function create(values) {
-        axios.post(`${BASE_URL}/billingCycles`, values)
-            .then(resp => {
-                toastr.success('Sucesso', 'Operação realizada com sucesso.')
-            })
-            .catch(e => {
-                e.response.data.erros.forEach(error => toastr.error('Erro', erro))
-            })
-        return {
-            type: 'TEMP'
+        return dispatch => {
+            axios.post(`${BASE_URL}/billingCycles`, values)
+                .then(resp => {
+                    toastr.success('Sucesso', 'Operação realizada com sucesso.')
+                    dispatch([
+                        resetForm('billingCycleForm'),
+                        getList(),
+                        selectTab('tabList'),
+                        showTabs('tabList', 'tabCrete')
+                    ])
+                })
+                .catch(e => {
+                    e.response.data.erros.forEach(error => toastr.error('Erro', erro))
+                })
         }
     }
 }
