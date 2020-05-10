2ef6a8cc33a29ee309234aa36e7507788797007c
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-10 19:52:18 -0300

    Login auth and validate token

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -12,4 +12,41 @@ const sendErrorsFromDB = (res, dbErrors) => {
         errors
     })
 }
-F
+
+const login = (req, res, next) => {
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    User.findOne({
+        email
+    }, (err, user) => {
+        if (err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign(user, env.authSecret, {
+                expiresIn: "1 day"
+            })
+            const {
+                name,
+                email
+            } = user
+            res.json({
+                name,
+                email,
+                token
+            })
+        } else {
+            return res.status(400).send({
+                errors: ['Usuário/Senha inválidos']
+            })
+        }
+    })
+}
+
+const validateToken = (req, res, next) => {
+    const token = req.body.token || ''
+    jwt.verify(token, env.authSecret, function (err, decoded) {
+        return res.status(200).send({
+            valid: !err
+        })
+    })
+}
