9caef489648ec4da2053a8d13a068c4767dae2c5
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-10 15:57:10 -0300

    Calculate sum credits and debts

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,10 +6,18 @@ import { reduxForm, Field, formValueSelector } from "redux-form";
 import { init } from "./billingCycleActions";
 import LabelAndInput from "../common/form/labelAndInput";
 import ItemList from "./itemList";
+import Summary from "./summary";
 
 class BillingCycleForm extends Component {
+  calculateSummary() {
+    const sum = (t, v) => t + v;
+    sumOfCredits: this.props.credits.map((c) => +c.value || 0).reduce(sum);
+    sumOfDebts: this.props.debts.map((d) => +d.value || 0).reduce(sum);
+  }
+
   render() {
     const { handleSubmit, readOnly, credits, debts } = this.props;
+    const { sumOfCredits, sumOfDebts } = this.calculateSummary();
     return (
       <form role="form" onSubmit={handleSubmit}>
         <div className="box-body">
@@ -38,6 +46,14 @@ class BillingCycleForm extends Component {
             label="Ano"
             cols="12 4"
             placeholder="Informe o ano"
+          />
+          <Summary credit={sumOfCredits} debt={sumOfDebts} />
+          <ItemList
+            cols="12 6"
+            list={credits}
+            readOnly={readOnly}
+            field="credits"
+            legend="Créditos"
           />
           <ItemList
             cols="12 6"
